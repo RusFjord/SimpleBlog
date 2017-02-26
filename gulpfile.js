be215a960415583ddafd6d068b2bba5d07a4c402
Fedor Endovitskiy <rusfjord@ulteam8.ru>
2017-02-26 14:23:48 +0300

    Added watch and directory with assets. index.html move to assets.

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,7 @@ const stylus = require('gulp-stylus');
 const sourcemaps = require('gulp-sourcemaps');
 const gulpIf = require('gulp-if');
 const del = require('del');
+const debug = require('gulp-debug');
 
 const isDevelopment = !process.env.NODE_ENV || process.env.NODE_ENV == 'development';
 
@@ -20,9 +21,19 @@ gulp.task('clean', function () {
     return del('public');
 });
 
-gulp.task('index-html', function () {
-    return gulp.src('frontend/index.html')
+gulp.task('assets', function () {
+    return gulp.src('frontend/assets/**/*.*', {since: gulp.lastRun('assets')})
+        .pipe(debug({title: 'assets'}))
         .pipe(gulp.dest('public'));
 });
 
-gulp.task('build', gulp.series('clean', gulp.parallel('index-html', 'styles')));
+gulp.task('build', gulp.series('clean', gulp.parallel('assets', 'styles')));
+
+gulp.task('watch', function () {
+    gulp.watch('frontend/styles/**/*.*', gulp.series('styles'));
+    gulp.watch('frontend/assets/**/*.*', gulp.series('assets'));
+});
+
+gulp.task('dev', gulp.series('build', 'watch'));
+
+gulp.task('production', gulp.series('build'));
